ccaa3cc4115ae71df8698db844996806b7a1e6c4
matteomorganti <73470272+matteomorganti@users.noreply.github.com>
2021-02-14 03:05:03 +0100

    modifiche design dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,25 +1,28 @@
+/* eslint-disable react/no-string-refs */
 import React, { Component } from "react";
 import axios from "axios";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import HomeIcon from "@material-ui/icons/Home";
 import BookIcon from "@material-ui/icons/Book";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import PersonIcon from "@material-ui/icons/Person";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import FormControl from "@material-ui/core/FormControl";
 import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import PropTypes from "prop-types";
 import AuthenticationService from "./AuthenticationService";
 import "./css/Dashboard.css";
@@ -50,31 +53,6 @@ class Dashboard extends Component {
       )
       .then((response) => {
         console.log(response);
-      });
-  }
-
-  libriNonRestituiti() {
-    let JWTToken = localStorage.getItem("token");
-    axios
-      .get(
-        "http://g0ptrkwkej5fhqfl.myfritz.net:8090/api/prenotazioni/daRestituire/:utente",
-        { headers: { Authorization: `${JWTToken}` } }
-      )
-      .then((response) => {
-        console.log(JWTToken);
-      });
-  }
-
-  numeroLibriNoleggiati() {
-    let JWTToken = localStorage.getItem("token");
-    axios
-      .get(
-        "http://g0ptrkwkej5fhqfl.myfritz.net:8090/api/prenotazioni/numberLibriUtente/" +
-          utente,
-        { headers: { Authorization: `${JWTToken}` } }
-      )
-      .then((response) => {
-        console.log(JWTToken);
       });
   }
 
@@ -119,6 +97,44 @@ class Dashboard extends Component {
       .catch((error) => console.error(`Error:  ${error}`));
   };
 
+  addNewLibro(titolo, data, quantita, copertina, genere) {
+    let JWTToken = localStorage.getItem("token");
+    axios
+      .post("http://g0ptrkwkej5fhqfl.myfritz.net:8090/api/upload/caricaLibro", {
+        titolo,
+        data,
+        quantita,
+        copertina,
+        genere,
+        headers: { Authorization: `${JWTToken}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+        console.log(JWTToken);
+      })
+      .catch((error) => console.error(`Error:  ${error}`));
+    this.setState({ error: "Qualcosa è andato storto" });
+  }
+
+  doAddNewLibro = async (event) => {
+    event.preventDefault();
+    this.addNewLibro(
+      this.state.titolo,
+      this.state.data,
+      this.state.quantita,
+      this.state.copertina,
+      this.state.genere
+    ).then(
+      () => {
+        console.log(response.data);
+      },
+      (error) => {
+        console.log("Uploading fail error = { " + error.toString() + " }");
+        this.setState({ error: "Inserimento non riuscito" });
+      }
+    );
+  };
+
   doLogout = async (event) => {
     event.preventDefault();
     AuthenticationService.logOut();
@@ -136,28 +152,24 @@ class Dashboard extends Component {
               </ListItemIcon>
               <ListItemText primary="Home" />
             </ListItem>
-            <ListItem button>
+            <ListItem button onClick={this.getAllLibri}>
               <ListItemIcon>
-                <BookIcon className={"menuIcon"}/>
+                <BookIcon className={"menuIcon"} />
               </ListItemIcon>
-              <ListItemText primary="Libri" />
+              <ListItemText primary="Lista libri" />
             </ListItem>
             <ListItem button onClick={this.getUser}>
               <ListItemIcon>
-                <PersonIcon className={"menuIcon"}/>
+                <PersonIcon className={"menuIcon"} />
               </ListItemIcon>
-              <ListItemText primary="Lista Utenti" />
+              <ListItemText primary="Lista utenti" />
             </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItem button onClick={this.doLogout}>
-                  <ListItemIcon>
-                    <ExitToAppIcon className={"menuIcon"}/>
-                  </ListItemIcon>
-                  <ListItemText primary="Logout" />
-                </ListItem>
-              </List>
-            </Collapse>
+            <ListItem button onClick={this.doLogout}>
+              <ListItemIcon>
+                <ExitToAppIcon className={"menuIcon"} />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItem>
           </List>
         </div>
         <div className="main">
@@ -185,6 +197,110 @@ class Dashboard extends Component {
               </Table>
             </TableContainer>
           </div>
+          <div style={{ padding: "2rem" }}>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Titolo</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.book.map((libro) => (
+                    <TableRow key="book">
+                      <TableCell>{libro.ID}</TableCell>
+                      <TableCell>{libro.Titolo}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          <FormControl
+            noValidate
+            autoComplete="off"
+            onSubmit={this.doAddNewLibro}
+          >
+            <div>
+              <TextField
+                variant="outlined"
+                type="text"
+                name="titolo"
+                id="titolo"
+                value={this.state.titolo}
+                label="Titolo"
+                autoComplete="titolo"
+                onChange={this.changeHandler}
+                style={{ marginTop: "25px", marginBottom: "10px" }}
+              />
+            </div>
+
+            <div>
+              <TextField
+                variant="outlined"
+                type="text"
+                name="data"
+                id="data"
+                placeholder="GG/MM/AAAA"
+                label="Data"
+                value={this.state.data}
+                autoComplete="data"
+                style={{ marginTop: "10px", marginBottom: "10px" }}
+              />
+            </div>
+            <div>
+              <TextField
+                variant="outlined"
+                type="text"
+                name="quantita"
+                id="quantita"
+                label="Quantità"
+                value={this.state.quantita}
+                autoComplete="quantita"
+                style={{ marginTop: "10px", marginBottom: "10px" }}
+              />
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                component="label"
+                style={{
+                  backgroundColor: "white",
+                  marginTop: "10px",
+                  marginBottom: "30px",
+                }}
+              >
+                Carica Copertina
+                <input type="file" hidden />
+              </Button>
+            </div>
+            <div>
+              <TextField
+                variant="outlined"
+                type="text"
+                name="genere"
+                id="genere"
+                label="Genere"
+                value={this.state.genere}
+                autoComplete="genere"
+                style={{ marginTop: "10px", marginBottom: "10px" }}
+              />
+            </div>
+
+            <Button
+              variant="contained"
+              type="submit"
+              style={{
+                color: "whitesmoke",
+                backgroundColor: "#006ddb",
+                marginTop: "10px",
+                marginBottom: "30px",
+              }}
+            >
+              Invia
+            </Button>
+          </FormControl>
         </div>
       </div>
     );
--- src/Login.js
+++ src/Login.js
@@ -45,6 +45,7 @@ class Login extends Component {
       }
     );
   };
+  
 
   render() {
     return (
